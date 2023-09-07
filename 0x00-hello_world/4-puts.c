7c849bb7bd87e9fe4ecd085ea0b15e457ab974a7
LisetteMurugi <lisettendwiga4@gmail.com>
2023-09-07 15:06:30 -0700

    four

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,9 +1,10 @@
-#include <studio.hi>
+#include<stdio.h>
 /**
  * main - Entry point
  * Return: 0 (Success)
  */
 int main(void)
-{put("\"Programming is like building a multilingual puzzle");
+{
+put("\"Programming is like building a multilingual puzzle");
 return (0);
 }
