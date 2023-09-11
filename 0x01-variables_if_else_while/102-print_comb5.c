83513164ba1f8a9f466bea4ba25999d340a7704c
LisetteMurugi <lisettendwiga4@gmail.com>
2023-09-11 13:44:18 -0700

    kll

--- 0x01-variables_if_else_while/102-print_comb5.c
+++ 0x01-variables_if_else_while/102-print_comb5.c
@@ -6,20 +6,20 @@
  */
 int main(void)
 {
-	int i, j;
+	int p, q;
 
-	for (i = 0; i < 100; i++)
+	for (p = 0; p < 100; p++)
 	{
-		for (j = 0; j < 100; j++)
+		for (q = 0; q < 100; q++)
 		{
-			if (i < j)
+			if (p < q)
 			{
-				putchar((i / 10) + 48);
-				putchar((i % 10) + 48);
+				putchar((p / 10) + 48);
+				putchar((p % 10) + 48);
 				putchar(' ');
-				putchar((j / 10) + 48);
-				putchar((j % 10) + 48);
-				if (i != 98 || j != 99)
+				putchar((q / 10) + 48);
+				putchar((q % 10) + 48);
+				if (p != 98 || q != 99)
 				{
 					putchar(',');
 					putchar(' ');
