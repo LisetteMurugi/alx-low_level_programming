135fa894eb01f3a76eb81798a2c330f80abf2edb
LisetteMurugi <lisettendwiga4@gmail.com>
2023-09-11 13:09:07 -0700

    ggg

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -6,17 +6,17 @@
  */
 int main(void)
 {
-	int n, m;
+	int n, k;
 
 	for (n = 48; n <= 56; n++)
 	{
-		for (m = 49; m <= 57; m++)
+		for (k = 49; k <= 57; m++)
 		{
-			if (m > n)
+			if (k > n)
 			{
 				putchar(n);
-				putchar(m);
-				if (n != 56 || m != 57)
+				putchar(k);
+				if (n != 56 || k != 57)
 				{
 					putchar(',');
 					putchar(' ');
