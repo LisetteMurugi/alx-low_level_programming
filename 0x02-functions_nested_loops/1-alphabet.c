20601dfbc275b45bd6913f701405fc954fb73cde
LisetteMurugi <lisettendwiga4@gmail.com>
2023-09-12 12:53:11 -0700

    alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,19 @@
+#include "main.h"
+
+
+/**
+* print_alphabet - print alphabet in lowercase as stated in task.
+*/
+
+
+void print_alphabet(void)
+{
+char letter;
+
+
+for (letter = 'a'; letter <= 'z'; letter++)
+_putchar(letter);
+
+
+_putchar('\n');
+}
